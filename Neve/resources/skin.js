9b71f6db4e2d8368429668b9e85b0ff3aad28dcb
Jon Robson <jdlrobson@gmail.com>
2022-12-26 16:24:47 -0800

    Uses newer version of mediawiki-skins-cli

--- Neve/resources/skin.js
+++ Neve/resources/skin.js
@@ -1,3 +1,36 @@
+mw.requestIdleCallback( function () {
+	// History URL
+	$('[href="?action=history"]').prop('href', mw.util.getUrl( mw.config.get('wgTitle'), { action: 'history' } ) );
+} );/** Polyfills for MediaWiki <= 1.40 */
+mw.requestIdleCallback( function () {
+	/* Polyfill for LastModifiedLine */
+	var $polyfillLastMod = $('.skin-polyfill-last-modified');
+	if ( !$polyfillLastMod.length) {
+		return;
+	}
+	mw.loader.using( 'mediawiki.api' ).then( function () {
+		var api = new mw.Api();
+		api.get( {
+			action: 'query',
+			prop: 'revisions',
+			titles: mw.config.get('wgTitle'),
+			formatversion: 2,
+			redirects: 1
+		} ).then( function ( a ) {
+			var lastmod;
+			try {
+				lastmod = new Date( a.query.pages[0].revisions[0].timestamp );
+				lastmod = lastmod.toLocaleDateString(
+					mw.config.get('wgUserLanguage'),
+					{ year:"numeric", month:"short", day:"numeric" }
+				);
+			} catch ( e ) {
+				lastmod = 'Unknown';
+			}
+			$polyfillLastMod.replaceWith(lastmod);
+		} );
+	} );
+} );
 // Disable module exporting in this context
 module = undefined; exports = undefined;
 window.NeveProperties = {ajaxurl: "", nonce: "", isRTL: false, isCustomize: false};
